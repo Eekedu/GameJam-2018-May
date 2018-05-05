fe952118ccc62cf0ccf34e98938daa69ab8a95ba
Kyttynz Tehkat <kyttynz@gmail.com>
2018-05-05 14:39:16 -0600

    ActivePlayer class to track players

--- Assets/assets/scripts/RoundManager.cs
+++ Assets/assets/scripts/RoundManager.cs
@@ -19,7 +19,7 @@ public class RoundManager : MonoBehaviour {
     private int m_iPlayerCount;
     private PlayerHUDScript[] m_aoPlayerHUDs;
     private CameraScript m_oGameCamera;
-    private playerEffector[] m_oActivePlayers;
+    private ActivePlayer[] m_oActivePlayers;
 
 
 
@@ -30,6 +30,23 @@ public class RoundManager : MonoBehaviour {
         public TokenScript m_oObject;
         public bool m_bSpawning;
         public float m_fSpawnTime;
+    }
+
+    private class ActivePlayer
+    {
+        public GameObject m_oObject;
+        public playerEffector m_oEffector;
+        public playerController m_oController;
+        public ActivePlayer(GameObject playob)
+        {
+            m_oObject = playob;
+            m_oController = playob.GetComponent<playerController>();
+            m_oEffector = playob.GetComponent<playerEffector>();
+        }
+        public Vector2 GetPosition()
+        {
+            return new Vector2(m_oObject.transform.position.x, m_oObject.transform.position.y);
+        }
     }
 
     private enum Phase
@@ -51,7 +68,7 @@ public class RoundManager : MonoBehaviour {
         ConvertPlayerSpawns();
         ConvertTokenSpawns();
         EnterPhaseStart();
-        SetPlayerCount(4);
+        SetPlayerCount(2);
         m_oGameCamera = GetComponentInChildren<CameraScript>();
         
 
@@ -73,7 +90,7 @@ public class RoundManager : MonoBehaviour {
             m_aoPlayerHUDs[i] = Instantiate(m_preHUD, this.transform);
             m_aoPlayerHUDs[i].transform.localPosition = new Vector3(fXPos, fYStart);
         }
-        m_oActivePlayers = new playerEffector[count];
+        m_oActivePlayers = new ActivePlayer[count];
     }
 
     private void EnterPhaseStart()
@@ -144,7 +161,7 @@ public class RoundManager : MonoBehaviour {
 
     private void SpawnPlayer(int playerdex)
     {
-        m_oActivePlayers[0] = Instantiate(playerPrefab, new Vector3(m_v2PlayerSpawns[0].x, m_v2PlayerSpawns[0].y), Quaternion.identity).GetComponent<playerEffector>();
+        m_oActivePlayers[0] = new ActivePlayer(Instantiate(playerPrefab, new Vector3(m_v2PlayerSpawns[0].x, m_v2PlayerSpawns[0].y), Quaternion.identity));
         m_aoPlayerHUDs[0].SetHealth(100, false);
     }
 
@@ -193,9 +210,9 @@ public class RoundManager : MonoBehaviour {
     private Vector2 m_vTopLeftBound, m_vBottomRightBound;
     private void UpdatePlayerBounds()
     {
-        m_vTopLeftBound = new Vector2(Mathf.Min(m_oActivePlayers[0].gameObject.transform.position.x, 0), Mathf.Max(m_oActivePlayers[0].gameObject.transform.position.y, 0));
-        m_vBottomRightBound.x = Mathf.Max(m_oActivePlayers[0].gameObject.transform.position.x, 0);
-        m_vBottomRightBound.y = Mathf.Min(m_oActivePlayers[0].gameObject.transform.position.y, 0);
+        m_vTopLeftBound = new Vector2(Mathf.Min(m_oActivePlayers[0].GetPosition().x, 0), Mathf.Max(m_oActivePlayers[0].GetPosition().y, 0));
+        m_vBottomRightBound.x = Mathf.Max(m_oActivePlayers[0].GetPosition().x, 0);
+        m_vBottomRightBound.y = Mathf.Min(m_oActivePlayers[0].GetPosition().y, 0);
         Debug.Log(m_vTopLeftBound.ToString() + " to " + m_vBottomRightBound.ToString());
     }
 
