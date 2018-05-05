edcd51735a9120fcf63b407faa64a0757fc470b6
Kyttynz Tehkat <kyttynz@gmail.com>
2018-05-05 11:15:03 -0600

    Setting up Splash scene

--- Assets/assets/scripts/SceneBoss.cs
+++ Assets/assets/scripts/SceneBoss.cs
@@ -1,16 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+public class SceneBoss : MonoBehaviour
+{
+    public static SceneBoss g_oSceneBoss;
+    private Image m_oFadeUIImage;
+    public float m_fFadeSpeed = 0.8f;
+    public enum FadeDirection
+    {
+        FD_In, //Alpha = 1
+        FD_Out // Alpha = 0
+    }
+    public enum SceneSelect
+    {
+        SS_Null,
+        SS_Splash,
+        SS_Title
+    }
 
-public class SceneBoss : MonoBehaviour {
+    public void FadeOut()
+    {
+        StartCoroutine(Fade(FadeDirection.FD_Out));
+    }
+    public void FadeIn()
+    {
+        StartCoroutine(Fade(FadeDirection.FD_In));
+    }
+    private void Start()
+    {
+        if (g_oSceneBoss != null) return;
+        g_oSceneBoss = this;
+        DontDestroyOnLoad(this);
+        m_oFadeUIImage = GetComponentInChildren<Image>();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int GetSceneIndex(SceneSelect scene)
+    {
+        switch (scene)
+        {
+            case SceneSelect.SS_Null: return -1;
+        }
+
+        return -1;
+    }
+
+    private IEnumerator Fade(FadeDirection fadeDirection)
+    {
+        float alpha = (fadeDirection == FadeDirection.FD_Out) ? 1 : 0;
+        float fadeEndValue = (fadeDirection == FadeDirection.FD_Out) ? 0 : 1;
+        if (fadeDirection == FadeDirection.FD_Out)
+        {
+            while (alpha >= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+            m_oFadeUIImage.enabled = false;
+        }
+        else
+        {
+            m_oFadeUIImage.enabled = true;
+            while (alpha <= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+        }
+    }
+    private void SetColorImage(ref float alpha, FadeDirection fadeDirection)
+    {
+        m_oFadeUIImage.color = new Color(m_oFadeUIImage.color.r, m_oFadeUIImage.color.g, m_oFadeUIImage.color.b, alpha);
+        alpha += Time.deltaTime * (1.0f / m_fFadeSpeed) * ((fadeDirection == FadeDirection.FD_Out) ? -1 : 1);
+    }
+
 }
